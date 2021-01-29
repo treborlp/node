cb25959f2490c387a5fd7b48758fbeb53f95aa19
Robert Alvarado <treborlp@gmail.com>
2021-01-28 23:08:53 -0500

    Recibir informacion desde linea de comandos

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,5 +1,10 @@
 const { calcularTabla } = require('./multiplicar/multiplicar')
 
-calcularTabla("abc")
+//console.log(process.argv);
+let argv = process.argv[2];
+
+let base = argv.split("=")[1];
+
+calcularTabla(base)
     .then(archivo => console.log(`${archivo} creado`))
     .catch(errores => console.log(errores))
